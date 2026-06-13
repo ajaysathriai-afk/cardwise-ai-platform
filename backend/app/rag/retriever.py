fd26ef5ffe879ef8a631fe60b6a31106dbd4ff6d
ajay kumar sathri <98929671+ajsathri@users.noreply.github.com>
2026-06-13 18:22:09 +0530

    Optimize RAG memory usage

--- backend/app/rag/retriever.py
+++ backend/app/rag/retriever.py
@@ -1,24 +1,30 @@
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
+import os
 
 DB_FOLDER = "vector_db"
 
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
 
-import os
+def get_vectordb():
 
-print("CURRENT DIR =", os.getcwd())
-print("DB FOLDER =", DB_FOLDER)
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
 
-vectordb = Chroma(
-    persist_directory=DB_FOLDER,
-    embedding_function=embeddings
-)
+    print("CURRENT DIR =", os.getcwd())
+    print("DB FOLDER =", DB_FOLDER)
+
+    vectordb = Chroma(
+        persist_directory=DB_FOLDER,
+        embedding_function=embeddings
+    )
+
+    return vectordb
 
 
 def search_docs(query):
+
+    vectordb = get_vectordb()
 
     print("SEARCHING:", query)
 
